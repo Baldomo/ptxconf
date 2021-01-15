7f06d64a19589e70182dfe6e670cb8d05d90d99b
Baldomo <leobaldin.2000@gmail.com>
2021-01-15 17:21:11 +0100

    Updated api2md to use pdoc3 with straight html generation

--- api2md.py
+++ api2md.py
@@ -1,5 +1,4 @@
-#! /usr/bin/python
-
+#!/usr/bin/python
 import pdoc
 import sys
 import os
@@ -7,37 +6,44 @@ import os
 modpath = os.path.dirname(os.path.abspath(sys.argv[1]))
 modname = os.path.basename(sys.argv[1]).split(".py")[0]
 
+context = pdoc.Context()
+modules = [pdoc.Module(mod, context=context) for mod in sys.argv[1:]]
+pdoc.link_inheritance(context)
 
-sys.path.append(modpath)
-pdoc.import_path.append(modpath)
+def recursive_html(mod: pdoc.Module):
+    yield mod.name, mod.html()
+    for submod in mod.submodules():
+        yield from recursive_html(submod)
 
-md = []
+for mod in modules:
+    for modname, modhtml in recursive_html(mod):
+        print(modhtml)
 
-for line in pdoc.text(modname).split("\n"):
-    md.append(line)
-md.append("")
+# for line in pdoc.text(modname).split("\n"):
+#     md.append(line)
+# md.append("")
 
-for i, line in enumerate(md):
-    if i > len(md) - 2:
-        break
-    # first level headings
-    if "----" == md[i + 1][0:4]:
-        print "#" + line
+# for i, line in enumerate(md):
+#     if i > len(md) - 2:
+#         break
+#     # first level headings
+#     if "----" == md[i + 1][0:4]:
+#         print("# " + line)
 
-    # first level definitions class names / functions
-    elif (
-        line != ""
-        and "    " != line[0:4]
-        and "----" != line[0:4]
-        and "----" != md[i + 1][0:4]
-    ):
-        print "##" + line
+#     # first level definitions class names / functions
+#     elif (
+#         line != ""
+#         and "    " != line[0:4]
+#         and "----" != line[0:4]
+#         and "----" != md[i + 1][0:4]
+#     ):
+#         print("## " + line)
 
-    elif "    " == line[0:4] and "----" in md[i + 1][4:8]:
-        print "###" + line[4:]
-    # methods
-    elif "(self" in line:
-        print "####" + line[4:]
-    # plain text/members
-    elif line != "" and "    " == line[0:4] and "----" not in line:
-        print line[4:].strip()
+#     elif "    " == line[0:4] and "----" in md[i + 1][4:8]:
+#         print("### " + line[4:])
+#     # methods
+#     elif "(self" in line:
+#         print("#### " + line[4:])
+#     # plain text/members
+#     elif line != "" and "    " == line[0:4] and "----" not in line:
+#         print(line[4:].strip())
